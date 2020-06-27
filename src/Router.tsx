ab6afa5815473516eefe528d8705495b8c235812
illemonati <tonymiaotong@tioft.tech>
2020-06-27 16:53:46 -0700

    diseases search

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,51 @@
-import * as React from 'react';
-import {lazy, Suspense} from 'react';
-import riskCalculatorConfigs from './configs/riskCalculatorConfig.json';
-import questions from './configs/questions.json';
-import { Switch, Route } from 'react-router-dom';
-import pageLinks from './configs/links.json';
+import * as React from "react";
+import { lazy, Suspense } from "react";
+import riskCalculatorConfigs from "./configs/riskCalculatorConfig.json";
+import questions from "./configs/questions.json";
+import { Switch, Route } from "react-router-dom";
+import pageLinks from "./configs/links.json";
 
-
-const LifeStyleComponent = lazy(() => import('./modules/LifeStyle/LifeStyleComponent'));
-const HealthComponent = lazy(() => import('./modules/Health/HealthComponent'));
-const SettingsComponent = lazy(() => import('./modules/Settings/SettingsComponent'));
-const LifeStyleResourcesComponent = lazy(() => import('./modules/LifeStyle/LifeStyleResources/LifeStyleResourcesComponent'));
-const NearMeComponent = lazy(() => import('./modules/Health/NearMe/NearMeComponent'));
-const RiskCalculatorComponent = lazy(() => import('./modules/RiskCalculator/RiskCalculatorComponent'));
-const HomePageComponent = lazy(() => import('./modules/HomePage/HomePageComponent'));
-const SurveyComponent = lazy(() => import('./modules/Survey/SurveyComponent'));
-const MyProfileComponent = lazy(() => import('./modules/Settings/MyProfile/MyProfileComponent'));
-const MapComponent = lazy(() => import('./modules/Map/MapComponent'));
-const FoodBanksMapComponent = lazy(() => import('./modules/Map/SpecificMaps/FoodBanksComponent'));
-const FreeClinicsMapComponent = lazy(() => import('./modules/Map/SpecificMaps/FreeClinicsComponent'));
-const SystemConfigurationComponent = lazy(() => import('./modules/Settings/SystemConfiguration/SystemConfigurationComponent'));
-const OssAttributionsComponent = lazy(() => import('./modules/Settings/OssAttributions/OssAttributionsComponent'));
-
-
+const LifeStyleComponent = lazy(() =>
+    import("./modules/LifeStyle/LifeStyleComponent")
+);
+const HealthComponent = lazy(() => import("./modules/Health/HealthComponent"));
+const SettingsComponent = lazy(() =>
+    import("./modules/Settings/SettingsComponent")
+);
+const LifeStyleResourcesComponent = lazy(() =>
+    import("./modules/LifeStyle/LifeStyleResources/LifeStyleResourcesComponent")
+);
+const NearMeComponent = lazy(() =>
+    import("./modules/Health/NearMe/NearMeComponent")
+);
+const RiskCalculatorComponent = lazy(() =>
+    import("./modules/RiskCalculator/RiskCalculatorComponent")
+);
+const HomePageComponent = lazy(() =>
+    import("./modules/HomePage/HomePageComponent")
+);
+const SurveyComponent = lazy(() => import("./modules/Survey/SurveyComponent"));
+const MyProfileComponent = lazy(() =>
+    import("./modules/Settings/MyProfile/MyProfileComponent")
+);
+const MapComponent = lazy(() => import("./modules/Map/MapComponent"));
+const FoodBanksMapComponent = lazy(() =>
+    import("./modules/Map/SpecificMaps/FoodBanksComponent")
+);
+const FreeClinicsMapComponent = lazy(() =>
+    import("./modules/Map/SpecificMaps/FreeClinicsComponent")
+);
+const SystemConfigurationComponent = lazy(() =>
+    import(
+        "./modules/Settings/SystemConfiguration/SystemConfigurationComponent"
+    )
+);
+const OssAttributionsComponent = lazy(() =>
+    import("./modules/Settings/OssAttributions/OssAttributionsComponent")
+);
+const DiseasesSearchComponent = lazy(() =>
+    import("./modules/Health/DiseasesSearch/DiseasesSearchComponent")
+);
 
 export default function RouterComponent() {
     return (
@@ -31,7 +55,7 @@ export default function RouterComponent() {
                 <Route path="/survey" excat>
                     <SurveyComponent
                         questions={questions}
-                        dbCollectionName={'demo-survey-0'}
+                        dbCollectionName={"demo-survey-0"}
                     />
                 </Route>
 
@@ -51,6 +75,9 @@ export default function RouterComponent() {
                 {/* Health Routes */}
                 <Route path="/health" exact>
                     <HealthComponent />
+                </Route>
+                <Route path="/health/diseases-search" exact>
+                    <DiseasesSearchComponent />
                 </Route>
                 <Route path="/health/near-me" exact>
                     <NearMeComponent />
